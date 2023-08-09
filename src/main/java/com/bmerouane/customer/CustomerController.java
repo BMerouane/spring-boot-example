ee4a2dbc8d2128300f7e1ba5b894fadb64a26dd5
Benamar Merouane <benamar.merouane@gmail.com>
2023-08-09 17:04:35 +0200

    Error handling with @ControllerAdvice & @ExceptionHandler

--- src/main/java/com/bmerouane/customer/CustomerController.java
+++ src/main/java/com/bmerouane/customer/CustomerController.java
@@ -2,11 +2,13 @@ package com.bmerouane.customer;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping(path = "api/v1/customers")
 public class CustomerController {
     private final CustomerService customerService;
 
@@ -14,12 +16,12 @@ public class CustomerController {
         this.customerService = customerService;
     }
 
-    @GetMapping(path = "api/v1/customers")
+    @GetMapping()
     public List<Customer> getCustomers() {
         return customerService.getAllCustomers();
     }
 
-    @GetMapping(path = "api/v1/customers/{id}")
+    @GetMapping(path = "/{id}")
     public Customer getCustomer(@PathVariable("id") Integer id) {
         return customerService.getCustomer(id);
     }
