725c9d06b94fdb766f0c185fd3db88cabe484bd4
Benamar Merouane <benamar.merouane@gmail.com>
2023-08-24 11:04:51 +0200

    PostgreSQL DB with Docker and Spring Data JPA

--- src/main/java/com/bmerouane/Main.java
+++ src/main/java/com/bmerouane/Main.java
@@ -1,11 +1,36 @@
 package com.bmerouane;
 
+import com.bmerouane.customer.Customer;
+import com.bmerouane.customer.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+    }
+
+    @Bean
+    CommandLineRunner runner(CustomerRepository customerRepository) {
+        return args -> {
+            Customer alexandre = new Customer(
+                    "Alexandre",
+                    "alexandre@gmail.com",
+                    26
+            );
+            Customer christopher = new Customer(
+                    "Christopher",
+                    "christopher@gmail.com",
+                    26
+            );
+
+            List<Customer> customers = List.of(alexandre, christopher);
+            customerRepository.saveAll(customers);
+        };
     }
 }
